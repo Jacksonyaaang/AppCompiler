04b45b13bc296a3f8fb37146666eff6e057cce8d
Equipe gl15 <gl15@gitlab.ensimag.fr>
2022-12-27 17:58:14 +0100

    Personalisation du dépôt pour gl15

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Projet Génie Logiciel, Ensimag.
-@AUTHOR@, @DATE@.
+gl15, 01/01/2023.
--- src/main/java/fr/ensimag/deca/tree/AbstractOpBool.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractOpBool.java
@@ -8,8 +8,8 @@ import fr.ensimag.deca.context.EnvironmentExp;
 
 /**
  *
- * @author @AUTHOR@
- * @date @DATE@
+ * @author gl15
+ * @date 01/01/2023
  */
 public abstract class AbstractOpBool extends AbstractBinaryExpr {
 
