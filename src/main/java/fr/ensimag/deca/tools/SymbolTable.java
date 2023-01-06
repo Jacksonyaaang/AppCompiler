159df2274181919f25de233435355b315b821dde
ETHMANE <mohamed-lemine.ethmane@grenoble-inp.org>
2023-01-06 02:48:11 +0100

    mis à jour les fichiers contex et SymboleTable

--- src/main/java/fr/ensimag/deca/tools/SymbolTable.java
+++ src/main/java/fr/ensimag/deca/tools/SymbolTable.java
@@ -25,7 +25,8 @@ public class SymbolTable {
      * this Symbol. Otherwise, create a new Symbol and add it to the table.
      */
     public Symbol create(String name) {
-        throw new UnsupportedOperationException("Symbol creation");
+        return this.map.get(name);
+        //throw new UnsupportedOperationException("Symbol creation");
     }
 
     public static class Symbol {
