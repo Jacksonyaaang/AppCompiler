41dbc3f0ff253a2afae39cc743468ad7bf6f9750
Frikha Mehdi <Mehdi.Frikha@grenoble-inp.org>
2023-01-08 03:08:38 +0100

    Finalisation de la grammaire de Syntax et intégration des options du compilateur et ajoute de quelque test custom

--- customLaunchers/temptests/temp.sh
+++ customLaunchers/temptests/temp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "----test---"
-../../src/test/script/launchers/test_lex "$1"
+#../../src/test/script/launchers/test_lex "$1"
 ../../src/test/script/launchers/test_synt "$1"
-../../src/test/script/launchers/test_gen "$1"
+#../../src/test/script/launchers/test_gen "$1"
 
-#../../src/main/bin/decac "$1"
+../../src/main/bin/decac "$1"
 
