b27a6e72a77104c822bd791633bad26839c8dec5
Jiaxuan Yang <jiaxuan.yang@grenoble-inp.org>
2023-01-11 22:02:03 +0100

    les test automatique finis (pour auto les tests unitaires de  syntaxe[valide/personal et invalide/personal] et contex[valide/personal et invalide/personal]) faut ajouter les test unitaires sur les dossiers indiqués.   Utile  pour le report de  jacoco

---
+++ customLaunchers/TestContexuelle/autoConInvalide.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#one shell script to auto-test all the valide unity test for the part B
+cd "$(dirname "$0")"/../.. || exit 1
+
+PATH=./src/test/script/launchers/:"$PATH"
+#PATH=./src/test/deca/context/valid/provided:"$PATH"
+
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+THUMBS_UP='\U1F44D' 
+SMILE='\U1F60E' 
+SCR='\U1F631'
+GREENFOND="\e[42m"
+echo -e "${GREENFOND} AUTO INVALIDE CONTEXT TEST START : ${ENDCOLOR} \n" 
+
+test_context_invalide (){
+    if test_context "$1" 2>&1 | grep -q -e "$i:[0-9]*"
+    then
+        echo -e "${GREEN}TEST PASS! ${ENDCOLOR} ${THUMBS_UP}"    
+    else
+        echo -e "${RED}TEST NOT PASS!!  Issue file : $i ${SCR}"
+        exit 1
+    fi
+}
+
+for i in src/test/deca/context/invalid/personal/*.deca
+do
+    test_context_invalide "$i"
+done
---
+++ customLaunchers/TestContexuelle/autoConValide.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#one shell script to auto-test all the valide unity test for the part B
+cd "$(dirname "$0")"/../.. || exit 1
+
+PATH=./src/test/script/launchers/:"$PATH"
+#PATH=./src/test/deca/context/valid/provided:"$PATH"
+
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+THUMBS_UP='\U1F44D' 
+SMILE='\U1F60E' 
+SCR='\U1F631'
+
+GREENFOND="\e[42m"
+echo -e "${GREENFOND}AUTO VALIDE CONTEXT TEST START : ${ENDCOLOR} \n" 
+
+test_context_valide (){
+    if test_context "$1" 2>&1 | grep -q -e "$i:[0-9]*"
+    then
+        echo -e "${RED}TEST NOT PASS!!  Issue file : $i ${SCR}"
+        exit 1
+    else
+        echo -e "${GREEN}TEST PASS! ${ENDCOLOR} ${THUMBS_UP}"    
+    fi
+}
+
+for i in src/test/deca/context/valid/personal/*.deca
+do
+    test_context_valide "$i"
+done
