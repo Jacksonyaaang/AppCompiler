302688cc622701b71d3c4f11472f0fc90ae33c0e
Victor Cook <victorcook.off@gmail.com>
2023-01-12 00:57:17 +0100

    ajout de 3 nouveaux types d'erreurs dans ListError.java: div0_error missing_return_error deref_null_error

--- src/main/java/fr/ensimag/deca/codegen/ListError.java
+++ src/main/java/fr/ensimag/deca/codegen/ListError.java
@@ -10,11 +10,25 @@ public class ListError {
     //list[0]:statut de stack_overflow_error
     //list[1]:statut de overflow_error
     //list[2]:statut de io_error
+    //list[3]:statut de div0_error
+    //list[4]:statut de missing_return_error
+    //list[5]:statut de deref_null_error
+    
+    //io_error: Input/Output error: déclenchée quand un argument de méthode ou une 
+    //variable stockant l'objet retourné n'a pas le bon type.
+    //div0_error: déclenchée par une tentative de division par zéro.
+    //missing_return_error: déclenchée par l'absence de return dans une méthode non void.
+    //dereferencing_null_error: déclenchée par une tentative de déréférencement d'un objet null,
+    // i.e. d'utiliser un objet non instancié ou attribut d'objet non initialisé.
     private Map<String, Boolean> list = new HashMap<String, Boolean>();
     public ListError(){
         this.list.put("stack_overflow_error", false);
         this.list.put("overflow_error", false);
         this.list.put("io_error", false);
+        this.list.put("div0_error", false);
+        this.list.put("missing_return_error", false);
+        this.list.put("deref_null_error", false);
+
     }
 
     public void Active_Stack_Overflow_Error(String err_name){
@@ -25,18 +39,7 @@ public class ListError {
             this.list.replace(err_name, true);
         }
     }
-    
-    // public void Active_Stack_Overflow_Error(){
-    //     this.list.replace("stack_overflow_error", true);
-    // }
-    
-    // public void Active_Overflow_Error(){
-    //     this.list.replace("overflow_error", true);
-    // }
 
-    // public void Active_Io_Error(){
-    //     this.list.replace("io_error", true);    
-    // }
 
     public void WriteListError(DecacCompiler compiler){
         for(Map.Entry<String, Boolean> set : list.entrySet()){
