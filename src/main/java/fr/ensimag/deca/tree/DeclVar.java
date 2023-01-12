10ac81ff4450596cd0b190dee93b7c39427602f0
ETHMANE <mohamed-lemine.ethmane@grenoble-inp.org>
2023-01-12 09:52:46 +0100

    double decl corrigée

--- src/main/java/fr/ensimag/deca/tree/DeclVar.java
+++ src/main/java/fr/ensimag/deca/tree/DeclVar.java
@@ -41,7 +41,8 @@ public class DeclVar extends AbstractDeclVar {
             varName.setDefinition(VDf);
             try{localEnv.declare(varName.getName(), varName.getExpDefinition());
             } catch (EnvironmentExp.DoubleDefException e) {
-                System.out.println(e.getMessage());
+                //System.out.println(e.getMessage());
+                throw new ContextualError(e.getMessage(), getLocation());
         }
     }
 
