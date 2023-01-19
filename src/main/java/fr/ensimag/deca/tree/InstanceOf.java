a7f17906212ef0d0a58cf60696a36716c11d62ae
Your Name <you@example.com>
2023-01-19 14:16:07 +0100

    Première version de méthode codeGenInst d'InstanceOf.java à tester; nouveau test: test_instanceof.deca

--- src/main/java/fr/ensimag/deca/tree/InstanceOf.java
+++ src/main/java/fr/ensimag/deca/tree/InstanceOf.java
@@ -3,11 +3,24 @@ package fr.ensimag.deca.tree;
 import java.io.PrintStream;
 
 import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.codegen.CodeGenError;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.ima.pseudocode.GPRegister;
+import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.RegisterOffset;
+import fr.ensimag.ima.pseudocode.instructions.BEQ;
+import fr.ensimag.ima.pseudocode.instructions.BNE;
+import fr.ensimag.ima.pseudocode.instructions.BRA;
+import fr.ensimag.ima.pseudocode.instructions.CMP;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+import fr.ensimag.ima.pseudocode.instructions.PUSH;
+import fr.ensimag.ima.pseudocode.instructions.STORE;
+
+
 import org.apache.commons.lang.Validate;
 
 
@@ -34,8 +47,26 @@ public class InstanceOf extends AbstractExpr {
         this.typeInstance = typeInstance;
     }
 
-    protected void codeGenInst(DecacCompiler compiler){
-        this.expr.
+    protected void codeGenInst(DecacCompiler compiler) throws CodeGenError{
+        this.expr.codeGenInst(compiler);
+        GPRegister R = expr.getRegisterDeRetour();
+        GPRegister RType = this.LoadGencode(compiler, false);
+        Label loopbegin = new Label("loopbegin");
+        Label endtrue = new Label("endtrue");
+        Label endfalse = new Label("endfalse");
+        compiler.addInstruction(new LOAD(compiler.getTableDeMethodeCompiler().getAdresseTableDeMethod().get(typeInstance.getClassDefinition()), RType));
+        // ^=on récupère la position de la classe de typeInstance dans R2
+        compiler.addLabel(loopbegin);  //
+        compiler.addInstruction(new LOAD(new RegisterOffset(0, R), R));
+        compiler.addInstruction(new CMP(R,null));
+        compiler.addInstruction(new BEQ(endfalse));
+        compiler.addInstruction(new CMP(R,RType));
+        compiler.addInstruction(new BNE(loopbegin));
+        compiler.addInstruction(new LOAD(1, R));
+        compiler.addInstruction(new BRA(endtrue));
+        compiler.addLabel(endfalse);
+        compiler.addInstruction(new LOAD(0, R));
+        compiler.addLabel(endtrue);
     }
 
     @Override
