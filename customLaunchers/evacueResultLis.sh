d5f66e0f37e47d7f55b516ca187314e32477b275
Frikha Mehdi <Mehdi.Frikha@grenoble-inp.org>
2023-01-23 21:15:09 +0100

    test

--- customLaunchers/evacueResultLis.sh
+++ customLaunchers/evacueResultLis.sh
@@ -6,3 +6,9 @@ rm src/test/deca/context/valid/result/*.lis
 rm src/test/deca/context/invalid/result/*.lis
 rm src/test/deca/syntax/valid/result/*.lis
 rm src/test/deca/syntax/invalid/result/*.lis
+
+rm src/test/deca/extension/context/valid/result/*.lis
+rm src/test/deca/extension/context/invalid/result/*.lis
+rm src/test/deca/extension/syntax/valid/result/*.lis
+rm src/test/deca/extension/syntax/invalid/result/*.lis
+
